76fa6a1954c54eff64569729dac45e08ef1e92cc
danliew123 <169648609+danliew123@users.noreply.github.com>
2025-06-11 14:56:31 +0800

    Create Navbar

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Provider } from "./components/ui/provider.jsx"
+import { ChakraProvider } from '@chakra-ui/react'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
